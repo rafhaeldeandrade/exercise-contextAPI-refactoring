67c6d0c571cd9e8843f07fac6f341edf97b3f569
Rafhael Gomes <rafhael.andrade@hotmail.com.br>
2021-09-13 20:47:54 -0300

    exercise 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      }
+    }
+  }
+
+  moveCar = (color, side) => {
+    this.setState({
+      cars: { ...this.state.cars, [color]: side }
+    });
+  }
+
+  render() {
+    const { cars: { red, blue, yellow } } = this.state;
+    const contextValue = {
+      cars: {
+        red,
+        blue,
+        yellow,
+      },
+      moveCar: this.moveCar,
+    }
+    return (
+      <MyContext.Provider value={ contextValue } >
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
